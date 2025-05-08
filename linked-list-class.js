0ddc50883785514e1c13dfde7f2c683874ff1434
Hilman Fikry <hilman12fikry@gmail.com>
2025-05-08 11:38:56 +0700

    Complete all linked list functions

--- linked-list-class.js
+++ linked-list-class.js
@@ -12,9 +12,7 @@ class LinkedList {
             this.head = lastNode;
         } else {
             let node = this.head;
-            while(node.nextNode) {
-                node = node.nextNode;
-            }
+            while(node.nextNode) node = node.nextNode;
             node.nextNode = lastNode;
         }
     }
@@ -36,7 +34,60 @@ class LinkedList {
         return sizeCounter;
     }
 
-    
+    toHead() {
+        return this.head;
+    }    
+
+    tail() {
+        let node = this.head;
+        while(node.nextNode) node = node.nextNode;
+        return node;
+    }
+
+    at(index) {
+        let node = this.head;
+        for(let nodeIter = 0; nodeIter < index; nodeIter++) {
+            node = node.nextNode;
+        }
+        return node;
+    }
+
+    pop() {
+        let node = this.head;
+        while(node.nextNode.nextNode) node = node.nextNode;
+        node.nextNode = null;
+    }
+
+    contains(value) {
+        let node = this.head;
+        while(node) {
+            if(node.value === value) return true;
+            node = node.nextNode;
+        }
+        return false;
+    }
+
+    find(value) {
+        let node = this.head;
+        let nodeIter = 0;
+        while(node) {
+            if(node.value === value) return nodeIter;
+            node = node.nextNode;
+            nodeIter++;
+        }
+        return null;
+    }
+
+    toString() {
+        if(!this.head) return "";
+        let printedList = "";
+        let node = this.head;
+        while(node) {
+            printedList += "( " + node.value + " ) -> ";
+            node = node.nextNode;
+        }
+        return printedList += null;
+    }
 }
 
 export default LinkedList;
--- main.js
+++ main.js
@@ -11,3 +11,13 @@ list.append("snake");
 list.append("turtle");
 
 console.log(list.size());
+console.log(list.toString());
+console.log(list.find("cat"));
+console.log(list.find("cock"));
+console.log(list.contains("parrot"));
+console.log(list.contains("cock"));
+console.log(list.toHead());
+console.log(list.tail());
+console.log(list.at(3));
+list.pop();
+console.log(list.toString());
